c094af604166b2f695957fc869aad4554adcfa17
Prazol Malla <prajwalmalla16@gmail.com>
2022-12-13 22:58:31 +0545

    coding challenge 1

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max(amplitude) and return it
 
+/*
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
@@ -29,9 +30,10 @@ const calcTempAmplitude = function (temps) {
   return max - min;
 };
 
+
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
-
+*/
 // problem 2:
 // Function should now receive 2 arrays of temperature
 
@@ -42,6 +44,7 @@ console.log(amplitude);
 // 2) Breaking into sub-programs
 // - How to merge Array
 
+/*
 const calcTempAmplitudeNew = function (t1, t2) {
   const temps = t1.concat(t2);
   let max = temps[0];
@@ -58,3 +61,67 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) FIX
+    //value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
+  // B) FIND
+  console.log(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// A) IDENTIFY
+console.log(measureKelvin());
+
+//Using a Debugger
+
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = 0;
+  let min = 0;
+  for (let i = 1; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    // debugger;
+    if (curTemp < min) min = curTemp;
+    if (curTemp > max) max = curTemp;
+  }
+  console.log(min, max);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
+
+//? Challenge 1
+
+//  1) Understanding the problem
+//  - Should the format be same
+//  - Is the given data in order
+
+// 2) Breaking into sub-programs
+// - How to format the output
+// - How to put degree symbol
+
+const printForecast = function (arr) {
+  let output = '... ';
+  for (let i = 0; i < arr.length; i++) {
+    output = output + `${arr[i]} C in ${i + 1} days ... `;
+  }
+  return output;
+};
+
+const a = [17, 21, 23];
+console.log(printForecast(a));
